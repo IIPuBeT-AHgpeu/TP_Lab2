ccf33d65947f9bb6f93be524b5f19385a2dd6383
kiran2000 <kiran2000@yandex.ru>
2023-01-10 05:55:31 +0300

    Написал тесты, но они пока не работают.

--- TP_Lab2/Program.cs
+++ TP_Lab2/Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using TP_Lab2.Models;
 
 var builder = WebApplication.CreateBuilder(args);
--- TP_Lab2_Tests/ShopControllerTest.cs
+++ TP_Lab2_Tests/ShopControllerTest.cs
@@ -1,4 +1,7 @@
 using TPLab2;
+using Moq;
+using TP_Lab2.Models;
+using TP_Lab2.Controllers;
 
 namespace TP_Lab2_Tests
 {
@@ -8,14 +11,47 @@ namespace TP_Lab2_Tests
         [TestMethod]
         public void GetSaleTest_30000_IN_0_05_OUT()
         {
+            var controller = new ShopController
+                (new ShopContext
+                (new Microsoft.EntityFrameworkCore.DbContextOptions<ShopContext>())
+                );
+
+            float expected = 0.05F;
+            float actual = controller.GetSale(30000F);
+
+            Assert.AreEqual(expected, actual);
         }
         [TestMethod]
         public void GetPurchasesTest_person_have_purchases()
         {
+            var controller = new ShopController
+                (new ShopContext
+                (new Microsoft.EntityFrameworkCore.DbContextOptions<ShopContext>())
+                );
+
+            var person = new Person() { Id = 1, PurchasesSum = 50597.906F };
+
+            PurchaseViewModel expected = new PurchaseViewModel() { Sale = 0.05F, Sum = 8799, Result = 8359.05F, Products = new List<ProductInPurchase>() };
+            PurchaseViewModel actual = controller.GetPurchases(person);
+            actual.Products = new List<ProductInPurchase>();
+
+            Assert.AreEqual(expected, actual);
         }
         [TestMethod]
         public void GetPurchasesTest_person_dont_have_purchases()
         {
+            var controller = new ShopController
+                (new ShopContext
+                (new Microsoft.EntityFrameworkCore.DbContextOptions<ShopContext>())
+                );
+
+            var person = new Person() { Id = 4, PurchasesSum = 0 };
+
+            PurchaseViewModel expected = new PurchaseViewModel() { Sale = 0.01F, Sum = 0, Result = 0F, Products = new List<ProductInPurchase>() };
+            PurchaseViewModel actual = controller.GetPurchases(person);
+            actual.Products = new List<ProductInPurchase>();
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
