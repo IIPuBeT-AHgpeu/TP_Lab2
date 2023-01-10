ca0abf05498cfae305c1fcf452b866df72babcc9
kiran2000 <kiran2000@yandex.ru>
2023-01-10 04:49:28 +0300

    Добавил проект для тестирования.

---
+++ TP_Lab2_Tests/ShopControllerTest.cs
@@ -0,0 +1,21 @@
+using TPLab2;
+
+namespace TP_Lab2_Tests
+{
+    [TestClass]
+    public class ShopControllerTest
+    {
+        [TestMethod]
+        public void GetSaleTest_30000_IN_0_05_OUT()
+        {
+        }
+        [TestMethod]
+        public void GetPurchasesTest_person_have_purchases()
+        {
+        }
+        [TestMethod]
+        public void GetPurchasesTest_person_dont_have_purchases()
+        {
+        }
+    }
+}
